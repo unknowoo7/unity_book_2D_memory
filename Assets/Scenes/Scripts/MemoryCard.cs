b7aa4287f6324bc9d6a3685a21a2b323f3b3e25c
unknowoo7 <kazak1995kazak@yandex.ru>
2026-01-04 11:19:02 +0300

    [154] Открытие карты по щелчку

---
+++ Assets/Scenes/Scripts/MemoryCard.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+namespace Scenes.Scripts
+{
+    public class MemoryCard : MonoBehaviour
+    {
+        public void OnMouseDown()
+        {
+            Debug.Log("OnMouseDown");
+        }
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
